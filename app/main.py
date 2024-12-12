76bdf095f33d874d831a8a5240bce6aa92ffa9d9
dannydjr <dannydelgadojr@gmail.com>
2024-12-12 23:44:01 +0800

    Handle invalid commands

--- app/main.py
+++ app/main.py
@@ -1,13 +1,16 @@
 import sys
 
-
 def main():
+    valid_commands = []
+
     # Uncomment this block to pass the first stage
     sys.stdout.write("$ ")
 
     # Wait for user input
-    input()
+    command = input()
 
+    if command not in valid_commands:
+        print(f"{command}: not found")
 
 if __name__ == "__main__":
     main()
