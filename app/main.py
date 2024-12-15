7418be6297290b0a504c80004c4eb8943be77f92
Danny DJR <dannydelgadojr@gmail.com>
2024-12-16 05:28:54 +0800

    REPL loop

--- app/main.py
+++ app/main.py
@@ -3,14 +3,16 @@ import sys
 def main():
     valid_commands = []
 
-    # Uncomment this block to pass the first stage
-    sys.stdout.write("$ ")
+    while True:
 
-    # Wait for user input
-    command = input()
+        # Uncomment this block to pass the first stage
+        sys.stdout.write("$ ")
 
-    if command not in valid_commands:
-        print(f"{command}: not found")
+        # Wait for user input
+        command = input()
+
+        if command not in valid_commands:
+            print(f"{command}: not found")
 
 if __name__ == "__main__":
     main()
