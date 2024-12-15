779526e9dad7980897605c1877cf63ad169c2af3
Danny DJR <dannydelgadojr@gmail.com>
2024-12-16 06:08:33 +0800

    implement echo

--- app/main.py
+++ app/main.py
@@ -1,7 +1,15 @@
 import sys
 
+def exit(status):
+    if status in [0, 1]:
+        sys.exit(status)
+
+def echo(args: list):
+    print(" ".join(args))
+
+
 def main():
-    valid_commands = ["exit 0"]
+    valid_commands = ["exit"]
 
     while True:
 
@@ -9,18 +17,20 @@ def main():
         sys.stdout.write("$ ")
 
         # Wait for user input
-        command = input().strip()
+        command = input().strip().split()
 
         # match command:
         #     case "exit":
         #         command += " 0"
 
-        if command not in valid_commands:
+        if command[0] not in valid_commands:
             print(f"{command}: not found")
         else:
-            match command:
-                case "exit 0":
-                    sys.exit(0)
+            match command[0]:
+                case "exit":
+                    exit(command[1])
+                case "echo":
+                    echo(command[1:])
 
 if __name__ == "__main__":
     main()
