11b3d94e7861e08c74e4e73670c7268b24ab2894
Danny DJR <dannydelgadojr@gmail.com>
2024-12-16 08:01:49 +0800

    echo command update

--- app/main.py
+++ app/main.py
@@ -1,6 +1,25 @@
 import sys
 
-def exit(status):
+def evaluate(command: list):
+
+    valid_commands = ["exit", "echo"]
+
+    if command[0] not in valid_commands:
+            print(f"{command}: not found")
+    else:
+        match command[0]:
+            case "exit":
+                if len(command) > 1:
+                    exit(command[1])
+                else:
+                    exit(0)
+            case "echo":
+                if len(command) > 1:
+                    echo(command[1:])
+                else:
+                    echo("")
+1
+def exit(status: int):
     if status in [0, 1]:
         sys.exit(status)
 
@@ -9,8 +28,6 @@ def echo(args: list):
 
 
 def main():
-    valid_commands = ["exit"]
-
     while True:
 
         # Uncomment this block to pass the first stage
@@ -23,14 +40,8 @@ def main():
         #     case "exit":
         #         command += " 0"
 
-        if command[0] not in valid_commands:
-            print(f"{command}: not found")
-        else:
-            match command[0]:
-                case "exit":
-                    exit(command[1])
-                case "echo":
-                    echo(command[1:])
+        evaluate(command)
+        
 
 if __name__ == "__main__":
     main()
