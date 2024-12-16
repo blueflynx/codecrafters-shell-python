81ef1d8facb8b54c11838b4b225bfce343fd59c0
Danny DJR <blueflynx@gmail.com>
2024-12-16 11:19:38 +0800

    codecrafters submit [skip ci]

--- app/main.py
+++ app/main.py
@@ -1,62 +1,94 @@
 import sys
 import os
 
-def valid_command(command):
-    valid_commands = ["exit", "echo", "type", "ls", "abcd"]
 
+def valid_command(command):
+    valid_commands = ["exit", "echo", "type"]
     return True if command in valid_commands else False
 
-def evaluate(command: list):
-
-    
-
-    if not valid_command(command[0]):
-            invalid_command("".join(command[0]))
-    else:
-        match command[0]:
-            case "exit":
-                if len(command) > 1:
-                    exit(int(command[1]))
-                else:
-                    exit(0)
-            case "echo":
-                if len(command) > 1:
-                    echo(command[1:])
-                else:
-                    echo("")
-            case "type":
-                if len(command) > 1:
-                    type(command[1])
-
-def type(command: str):
-    all_vars = os.environ
-    paths = os.environ['PATH'].split(':')
-
-    print("allvars=", all_vars)
-    print("paths=", paths)
-    
-    if valid_command(command):
-        for path in paths:
-            # print("path=", path)
-            # print("split=", path.split('/'))
-            # print("last=", path.split('/')[-1])
-            if command[-1] == path.split('/')[-1]:
-                print(f"{command} is {path}")
-                return
-        print(f"{command} is a shell builtin")
-    else:
-        invalid_command(command)
 
 def invalid_command(command: str):
     print(f"{command}: not found")
+
+
+def evaluate(input_list: list):
+
+    if not valid_command(input_list[0]):
+            invalid_command("".join(input_list[0]))
+    else:
+        match input_list[0]:
+            case "exit":
+                if len(input_list) > 1:
+                    exit(int(input_list[1]))
+                else:
+                    exit(0)
+            case "echo":
+                if len(input_list) > 1:
+                    echo(input_list[1:])
+                else:
+                    echo("")
+            case "type":
+                if len(input_list) > 1:
+                    type(input_list[1])
+
 
 def exit(status: int):
     if status in [0, 1]:
         sys.exit(status)
 
+
 def echo(args: list):
     print(" ".join(args))
 
+
+def check_command_path(directory, command):
+    """
+    Checks all files under a given directory.
+
+    Args:
+        directory: The path to the directory.
+
+    Prints:
+        The path of each file found in the directory and its subdirectories.
+    """
+    # print("directory=", directory)
+    # print("command=", command)
+
+    for root, _, files in os.walk(directory):
+        for file in files:
+            if file == command:
+                file_path = os.path.join(root, file)
+                # print("file_path=", file_path)
+                return file_path
+
+
+def type(command: str):
+    # all_vars = os.environ
+    
+    paths = os.environ['PATH'].split(':')
+    builtin_commands = ["exit", "echo", "type"]
+
+    # print("allvars=", all_vars)
+    # print("paths=", paths)
+    
+    # if not valid_command(command):
+    #     invalid_command(command)
+    # else:
+
+    if command in builtin_commands:
+        print(f"{command} is a shell builtin")
+        return
+
+    for path in paths:
+        # print("path=", path)
+        # print("split=", path.split('/'))
+        # print("last=", path.split('/')[-1])
+        command_path = check_command_path(path, command)
+        if command_path:
+            print(f"{command} is {command_path}")
+            return
+    invalid_command(command)
+        
 
 def main():
     while True:
@@ -65,13 +97,14 @@ def main():
         sys.stdout.write("$ ")
 
         # Wait for user input
-        command = input().strip().split()
+        command = input()
+        input_list = command.strip().split()
 
         # match command:
         #     case "exit":
         #         command += " 0"
 
-        evaluate(command)
+        evaluate(input_list)
         
 
 if __name__ == "__main__":
